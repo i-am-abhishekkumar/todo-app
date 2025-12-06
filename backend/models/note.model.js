6948f3d205a76624301d2a510659904907cfbfbd
abhishek-kumar1012 <abhishek10122000@gmail.com>
2025-12-06 15:46:40 +0530

    removed auto-increment id using mongo object id

--- backend/models/note.model.js
+++ backend/models/note.model.js
@@ -25,19 +25,5 @@ const noteSchema=new mongoose.Schema(
     }   
 )
 
-// Auto-increment ID before saving
-noteSchema.pre('save', async function() {
-    if (this.isNew && !this.id) {
-        try {
-            const NoteModel = this.constructor;
-            const maxNote = await NoteModel.findOne().sort({ id: -1 }).exec();
-            this.id = maxNote && maxNote.id ? maxNote.id + 1 : 1;
-        } catch (error) {
-            console.error("Error in pre-save hook:", error);
-            this.id = 1; // Fallback to 1 if query fails
-        }
-    }
-});
-
 const Note=mongoose.model('Note',noteSchema);
 export default Note;
