a32545489b8661090b9eb15bb240f43709f7b1f2
abhishek-kumar1012 <abhishek10122000@gmail.com>
2025-12-06 15:00:32 +0530

    made some minor chages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 py-4 border-t border-gray-800">
       <div className="text-center text-sm">
-        © {new Date().getFullYear()} <span className="text-blue-400 font-semibold">NoteKeeper</span>. Made by Abhishek Kumar.
+        © {new Date().getFullYear()} <span className="text-blue-400 font-semibold">Techie-Blogs</span>. Made by Abhishek Kumar.
       </div>
     </footer>
   );
